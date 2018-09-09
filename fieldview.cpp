c0eca4cde14a44fd9b8457ee651d54e1b8d8daed
Mykola Niemtsov <mykola.niemtsov@gmail.com>
2018-09-09 18:31:22 +0300

    Change background color to lightgreen.

--- fieldview.cpp
+++ fieldview.cpp
@@ -3,6 +3,7 @@
 FieldView::FieldView(QGraphicsScene* scene, QWidget* parent):
     QGraphicsView(scene, parent)
 {
+    setStyleSheet("background:lightgreen;");
 }
 
 void FieldView::closeEvent(QCloseEvent *event)
